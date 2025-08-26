c54b900bddf2242d4708444536ea1a3eab5a3d6c
Voltra <25990549+Voltra@users.noreply.github.com>
2025-08-26 10:36:07 +0000

    Final tweaks for migration

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -17,7 +17,7 @@ use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Illuminate\Support\Str;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
-use Voltra\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
+use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
 use Voltra\FilamentNavigation\FilamentNavigationServiceProvider;
 
 class TestCase extends Orchestra
